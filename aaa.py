b86acb5116356829081fa74fcaace27dfedbc411
fei.huang <fei_20018@163.com>
2013-05-31 18:02:04 +0800

    frist file

---
+++ aaa.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import getopt
+import commands
+import re
+import time
+#import threading
+import thread 
+xpin=commands.getoutput("ping -c5 60.28.9.1")
+ms='time=\d+.\d+'
+print xpin
+print ms
+mstime=re.search(ms,xpin)
+print mstime
+if not mstime:
+        MS='timeout'
+        print 'timeout'
+else:
+        MS=mstime.group().split('=')[1]
+        print 'ss'
